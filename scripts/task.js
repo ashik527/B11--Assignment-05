d3dcd0b017e0ae03c1d9f6a76b994e5792bf9e0d
Ashik git project <ashikurrahmanshk@gmail.com>
2025-03-01 17:14:39 +0600

    added the alert function

---
+++ scripts/task.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let taskCount = 6;
+    const taskElement = document.querySelector('.update-task-number');
+    const buttons = document.querySelectorAll('.complete');
+    const activityLog = document.getElementById('activity-log');
+    const clearHistoryBtn = document.getElementById('history-btn');
+
+    // Get Updated time
+    function getTime(){
+        const now = new Date();
+        return now.toLocaleTimeString('en-US',{
+            hour: '2-digit',minute: '2-digit',second: '2-digit',hour12 : true
+        })
+    }
+
+    // Mark Complete
+    buttons.forEach((button)=>{
+        button.addEventListener('click', function(){
+            if(!button.disabled){
+                alert("Board Updated Successfully")
+                button.classList.add('opacity-15','pointer-events-none');
+                taskCount--;
+                taskElement.textContent = taskCount;
+
+                // add log entry to activity log
+                const logHistory = document.createElement('p');
+                logHistory.textContent = `Task "${button.dataset.taskName}" completed at ${getTime()}`;
+                logHistory.classList.add("font-bold","mb-4");
+                activityLog.appendChild(logHistory);
+
+                // clear history and reset buttons
+                clearHistoryBtn.addEventListener('click', function (){
+                    activityLog.innerHTML = " ";
+                    taskCount = 6;
+                    taskElement.textContent = taskCount;
+
+                    buttons.forEach((button)=>{
+                        button.classList.remove('opacity-15','pointer-events-none')
+                    })
+                })
+            }
+        })
+    })
+})
